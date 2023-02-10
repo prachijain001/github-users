ddecd9526099cd7b78f6e304103bec37d7b1d328
Prachi <prachijain200310@gmail.com>
2023-02-10 12:36:58 +0530

    completed implementing api requests

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import './App.css';
 import {useState, useEffect} from 'react';
 
 function App() {
+  const token = "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"
   const [users, setUsers] = useState([]);
   const [search, setSearch] = useState('');
   const [searchby, setSearchby] = useState('login');
 
+  const profileCards = (image,login,email) => {
+    return (
+      <div className="profilecard">
+        <img src={image} alt="profile" className='pfimg'/>
+        <h3>{login}</h3>
+        <p>{email}</p>
+      </div>
+    )
+  }
+
+
+
   useEffect(() => {
-    // fetch('https://api.github.com/users')
-    // .then(res => res.json())
-    // .then(data => {
-    //   setUsers(data);
-    //   console.log(data);
-    // })
+    
     console.log(searchby, search, users);
   }, []);
 
@@ -28,6 +36,13 @@ function App() {
 
   const handleSubmit = (e) => {
     console.log(searchby, search, users);
+    fetch('https://api.github.com/search/users?q='+search)
+    .then(res => res.json())
+    .then(data => {
+      setUsers(data.items);
+      console.log(data);
+    })
+    
     // console.log(searchby);
   }
 
@@ -44,6 +59,9 @@ function App() {
       <input type="text" name="search" id="search" placeholder="Search by login, name or email" onChange={handleSearch}/>
       <button type="button" id="searchbtn" onClick={handleSubmit}>Search</button>
 
+      </div>
+      <div className="profilecards">
+        {users.map(user => profileCards(user.avatar_url, user.login, user.email))}
       </div>
     </div>
   );
